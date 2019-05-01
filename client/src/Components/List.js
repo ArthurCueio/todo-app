b5e3cad7fff909873ba2df388f618f33c42d849d
Arthur Coelho <arthur.cueio@gmail.com>
2019-05-01 10:50:09 -0300

    Add basic styling to the list

--- client/src/Components/List.js
+++ client/src/Components/List.js
@@ -4,9 +4,24 @@ class List extends React.Component {
   render() {
     const rows = this.props.items.map( (x) => {
       return (
-        <li className="list-item" key={x._id}>
+        <li
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            borderBottom: 'solid black 2px',
+            padding: '0.5em',
+          }}
+          className="list-item"
+          key={x._id}>
           {x.title}
           <button
+            style={{
+              border: 'none',
+              background: 'transparent',
+              fontWeight: 'bold',
+              color: 'red',
+              cursor: 'pointer',
+            }}
             value={x._id}
             onClick={this.props.handleDeleteButton}
             className="delete-button">
@@ -15,7 +30,13 @@ class List extends React.Component {
         </li>);
     });
     return (
-      <div>
+      <div
+        style={{
+          margin: '2rem',
+          width: 650,
+          padding: '1em',
+          backgroundColor: '#ffeeaa',
+        }}>
         <h1>To-do list:</h1>
         <ul>
           {rows}
