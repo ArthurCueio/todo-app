d47e839d26426806773b08542bf2d9b91a5ffa66
Arthur Coelho <arthur.cueio@gmail.com>
2019-05-03 11:08:17 -0300

    Use new url parser

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 
 const apiRouter = require('./routes/api');
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
 
 const app = express();
 
